3e32de375420958b1922a778b58667b13dd6d8c0
Paweł Kozłowski <pawelk9@gmail.com>
2016-05-01 15:03:52 +0200

    prototype, submiting to collecton

---
+++ imports/ui/body.js
@@ -0,0 +1,28 @@
+import { Template } from "meteor/templating";
+
+import { Matches } from '../api/matchesCollection';
+import "./body.html";
+
+
+Template.body.helpers({
+    matches() {
+        return Matches.find({}, {sort: {createdAt: -1}});
+    }
+});
+
+Template.body.events({
+    "submit .new-match"(event) {
+        event.preventDefault();
+
+        const target = event.target;
+        const matchName = target.matchName.value;
+
+        Matches.insert({
+            matchName,
+            createdAt: new Date()
+        });
+
+        target.matchName.value = "";
+        console.log(matchName);
+    }
+});
